4d0ead68c3652d6f62d5e2235386d8d55110e258
Lou <antilou86@gmail.com>
2019-07-16 16:11:47 -0400

    WIP: used .map to create operator buttons

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -5,15 +5,26 @@ import React, { useState } from "react";
 //Import your array data to from the provided data file
 import { operators } from '../../../data';
 
-const Operators = (operators) => {
+const Operators = () => {
   // STEP 2 - add the imported data to state
   const [operatorsState, setOperatorsState] = useState(operators);
   
+  let operatorFunction = function(array) {
+    array.map(operatorObject, ()=>{
+      return (
+        <button>{operatorObject.value}</button>
+      )
+    })
+  }
+
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {operatorFunction(operators)}
     </div>
   );
 };
+
+export default Operators
