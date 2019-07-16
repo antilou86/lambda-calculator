42fcace1d79bda6c90d070649a38a6affcc8a393
Lou <antilou86@gmail.com>
2019-07-16 16:06:02 -0400

    WIP: used .map to create number buttons

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -5,14 +5,27 @@ import React, { useState } from "react";
 //Import your array data to from the provided data file
 import { numbers } from '../../../data';
 
-const Numbers = (numbers) => {
+const Numbers = () => {
   // STEP 2 - add the imported data to state
   const [numbersState, setNumbersState] = useState(numbers);
+
+  let numberFunction = function(array) {
+    array.map( num, () => {
+       return (
+         <button>{num}</button>
+       )
+    }
+    )
+  };
+
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {numberFunction(numbers)}
     </div>
   );
 };
+
+export default Numbers
